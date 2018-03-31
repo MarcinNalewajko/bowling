1c38070e5a3b53225a5739866eebb5d1fd3781de
Marcin Nalewajko <bedgaar@gmail.com>
2018-03-31 22:07:42 +0200

    Bowl 1.02
    2nd step of cleaning

--- Bowl.py
+++ Bowl.py
@@ -4,133 +4,154 @@ test3='5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5'
 test4='1- 2/ 1- 1/ x 23 x 23 1/ x x x'
 
 
+def x_in_frame1_9(throw_list, score, i):
+    score.append(10)
+    try:
+        if throw_list[i-1]=='x'and i>0:
+            score[i-1]+=10
+            try:    
+                if throw_list[i-2]=='x'and i>1:
+                    score[i-2]+=10
+            except IndexError:
+                pass
+    except IndexError:
+        pass
+    try:
+        if throw_list[i-1][1]=='/'and i>0:
+            score[i-1]+=10
+    except IndexError:
+        pass
+    return score
 
+def throw1_in_frame1_9(throw_list, score, i):
+    if throw_list[i][0]=='-':
+        score.append(0)
+    else:
+        score.append(int(throw_list[i][0]))
+        try:
+            if throw_list[i-1]=='x'and i>0:
+                score[i-1]+=(int(throw_list[i][0]))
+        except IndexError:
+            pass
+        try:    
+            if throw_list[i-2]=='x'and i>1 and throw_list[i-1]=='x':
+                score[i-2]+=(int(throw_list[i][0]))
+        except IndexError:
+            pass
+        try:
+            if throw_list[i-1][1]=='/'and i>0:
+                score[i-1]+=(int(throw_list[i][0]))
+        except IndexError:
+            pass
+    return score
+
+
+def throw2_in_frame1_9(throw_list, score, i):
+    if throw_list[i][1]=='-':
+        score[i]+=0
+    elif throw_list[i][1]=='/':
+        try:
+            if throw_list[i-1]=='x'and i>0:
+                score[i-1]+=(10-score[i])
+        except IndexError:
+            pass
+        score[i]=10
+    else:
+        score[i]+=int(throw_list[i][1])
+        try:
+            if throw_list[i-1]=='x'and i>0:
+                    score[i-1]+=int(throw_list[i][1])
+        except IndexError:
+            pass
+        else:
+            pass
+    return score
 
 def frames1_9(throw_list, score):
     for i in range(0,9):
         if throw_list[i]=='x':
-            score.append(10)
-            try:
-                if throw_list[i-1]=='x'and i>0:
-                    score[i-1]+=10
-                    try:    
-                        if throw_list[i-2]=='x'and i>1:
-                            score[i-2]+=10
-                    except IndexError:
-                        pass
-            except IndexError:
-                pass
-            try:
-                if throw_list[i-1][1]=='/'and i>0:
-                    score[i-1]+=10
-            except IndexError:
-                pass
+            x_in_frame1_9(throw_list, score, i)   
         else:
-            if throw_list[i][0]=='-':
-                score.append(0)
-            else:
-                score.append(int(throw_list[i][0]))
-                try:
-                    if throw_list[i-1]=='x'and i>0:
-                        score[i-1]+=(int(throw_list[i][0]))
-                except IndexError:
-                    pass
-                try:    
-                    if throw_list[i-2]=='x'and i>1 and throw_list[i-1]=='x':
-                        score[i-2]+=(int(throw_list[i][0]))
-                except IndexError:
-                    pass
-                try:
-                    if throw_list[i-1][1]=='/'and i>0:
-                        score[i-1]+=(int(throw_list[i][0]))
-                except IndexError:
-                    pass
-            if throw_list[i][1]=='-':
-                score[i]+=0
-            elif throw_list[i][1]=='/':
-                try:
-                    if throw_list[i-1]=='x'and i>0:
-                        score[i-1]+=(10-score[i])
-                except IndexError:
-                    pass
-                score[i]=10
-            else:
-                score[i]+=int(throw_list[i][1])
-                try:
-                    if throw_list[i-1]=='x'and i>0:
-                            score[i-1]+=int(throw_list[i][1])
-                except IndexError:
-                    pass
-
-                else:
-                    pass   
+            throw1_in_frame1_9(throw_list, score, i)
+            throw2_in_frame1_9(throw_list, score, i)  
     return score
 
 
 
-def frame10(throw_list, score):
-    if throw_list[9][0]=='x':
-        score.append(10)
+
+
+def x_in_frame10(throw_list, score):
+    score.append(10)
+    if throw_list[8]=='x':
+        score[8]+=10
+        if throw_list[7]=='x':
+            score[7]+=10
+    try:
+        if throw_list[8][1]=='/':
+                    score[8]+=10
+    except IndexError:
+        pass
+    if throw_list[10][0]=='x':
+        score[9]+=10
         if throw_list[8]=='x':
             score[8]+=10
-            if throw_list[7]=='x':
-                score[7]+=10
+        if throw_list[11][0]=='x': 
+            score[9]+=10
+        elif throw_list[11][0]=='-':
+            pass
+        else:
+            score[9]+=(int(throw_list[11]))
+    elif throw_list[10][0]=='-':
+        if throw_list[11][0]=='x': 
+            score[9]+=20
+        elif throw_list[11][0]=='-':
+            pass
+        else:
+            score[9]+=(int(throw_list[11]))
+    else:
+        score[9]+= (int(throw_list[10][0]))
+        if throw_list[10][1]=='/':
+            score[9]+=10-(int(throw_list[10][0]))
+        elif throw_list[10][1]=='-':
+            pass
+        else:
+            score[9]+= (int(throw_list[10][1]))
+        if throw_list[8]=='x':
+            score[8]+= (int(throw_list[11]))
+    return score
+
+def throw1_in_frame10(throw_list, score):
+    if throw_list[9][0]=='-':
+        score.append(0)
+    else:
+        score.append(int(throw_list[9][0]))
+        if throw_list[8][0]=='x':
+                score[8]+=(int(throw_list[9][0]))
         try:
             if throw_list[8][1]=='/':
-                        score[8]+=10
+                score[8]+=(int(throw_list[9][0]))
         except IndexError:
             pass
-        if throw_list[10][0]=='x':
+    return score
+def throw2_and3_in_frame10(throw_list, score):
+    if throw_list[9][1]=='/':
+        score[9]=10
+        if throw_list[8][0]=='x':
+                score[8]+=10-(int(throw_list[9][0]))
+        if throw_list[9][2]=='-':
+            pass
+        elif throw_list[9][2]=='x':
             score[9]+=10
-            if throw_list[8]=='x':
-                score[8]+=10
-            if throw_list[11][0]=='x': 
-                score[9]+=10
-            elif throw_list[11][0]=='-':
-                pass
-            else:
-                score[9]+=(int(throw_list[11]))
-        elif throw_list[10][0]=='-':
-            if throw_list[11][0]=='x': 
-                score[9]+=20
-            elif throw_list[11][0]=='-':
-                pass
-            else:
-                score[9]+=(int(throw_list[11]))
         else:
-            score[9]+= (int(throw_list[10][0]))
-            if throw_list[10][1]=='/':
-                score[9]+=10-(int(throw_list[10][0]))
-            elif throw_list[10][1]=='-':
-                pass
-            else:
-                score[9]+= (int(throw_list[10][1]))
+            score[9]+=(int(throw_list[9][2]))
+    return score
 
-
-            if throw_list[8]=='x':
-                score[8]+= (int(throw_list[11]))
+def frame10(throw_list, score):
+    if throw_list[9][0]=='x':
+        x_in_frame10(throw_list, score)
     else:
-        if throw_list[9][0]=='-':
-            score.append(0)
-        else:
-            score.append(int(throw_list[9][0]))
-            if throw_list[8][0]=='x':
-                    score[8]+=(int(throw_list[9][0]))
-            try:
-                if throw_list[8][1]=='/':
-                    score[8]+=(int(throw_list[9][0]))
-            except IndexError:
-                pass
-        if throw_list[9][1]=='/':
-            score[9]=10
-            if throw_list[8][0]=='x':
-                    score[8]+=10-(int(throw_list[9][0]))
-            if throw_list[9][2]=='-':
-                pass
-            elif throw_list[9][2]=='x':
-                score[9]+=10
-            else:
-                score[9]+=(int(throw_list[9][2]))
+        throw1_in_frame10(throw_list, score)
+        throw2_and3_in_frame10(throw_list, score)
     return score
 
 
